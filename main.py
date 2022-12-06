b8394fb9484db059f3632d59f93d0c3efa45ede3
kgulati <kgulati@illinois.edu>
2022-12-06 15:06:56 -0600

    Interface Changes - WIP

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import movie_recommender as rec
+import move_recommender as rec
 import pandas as pd
 import numpy as np
 import os
@@ -7,11 +7,10 @@ import re
 import PySimpleGUI as sg
 from tkinter import *
 from  tkinter import ttk
-import sys
 
 prep_df = pd.read_csv("prep_movie_list_original.csv", sep=',', encoding='latin-1')
 
-layout = [[sg.Text("Enter a Netflix movie's details to find similar titles!")],
+layout = [[sg.Text("Enter Netflix Movie Info to Find Similar Options!")],
           [sg.Text("All fields not required, please leave blank if not used")],
           [sg.Text("Title", size =(15, 1)), sg.Input()],
           [sg.Text("Genre", size =(15, 1)), sg.Input()],
@@ -24,16 +23,8 @@ event, values = window.read()
 values[0].strip()
 #print('Vals after strip', values[0])
 #List
-rec_list = []
-rec_list = rec.Recommend_Movies(values[0])
-
-if rec_list == -2:
-    sg.Popup(f"Movie {values[0]} not present in Netflix. Please try another one.")
-    window.close()
-    exit()
-
-
-reco_list = rec_list
+#rec_list = rec.Recommend_Movies(values[0])
+#reco_list = rec_list
 
 '''
 parameter 1:
@@ -47,13 +38,22 @@ parameter 3:
 '''
 errorKey = 0
 #inp_param = 'JAWS||'
-inp_param = values[0] + "|" + values[1] + "|" + values[2]
+
+inp_param = values[0].upper() + "|" + values[1].upper() + "|" + values[2].upper()
 print(inp_param)
 
 param1 = str(inp_param.split('|',1)[0])
 param2 = inp_param.split('|',1)[1].split('|',1)[0]
 param3 = inp_param.rsplit('|',1)[1]
 
+#print(param1)
+#print(param2)
+#print(param3)
+reco_list = []
+reco_list = rec.Recommend_Movies(param1)
+#for x in reco_list:
+#    print(x)
+    
 recolistdf = pd.DataFrame(reco_list)
 recolistdf = recolistdf.rename(columns={0: 'title'})
 
@@ -78,18 +78,23 @@ if len(param2) > 0:
 if len(param3) > 0:
     recolistdf = recolistdf.loc[recolistdf['cast'].str.lower().str.contains(param3.lower())]
 
-if recolistdf.empty:
-    sg.Popup(f"Filter options entered for {values[0]} does not have any matching recommendation. Please try another title.")
-    window.close()
-    exit()
-
 print(recolistdf.loc[:,"title"])
 
+final_list = []
+i = 1
+netflix_titles = pd.read_csv("10k_titles.csv")
+for movie in recolistdf['title']:
+    print(movie.lower().title())
+    movie_str = str(i) + ".      " + movie.lower().title()
+    final_list.append(movie_str)
+    print(re.sub("[^A-Za-z ]","",str(netflix_titles.loc[netflix_titles['title'] == movie.lower().title()]['overview'].values)))
+    final_list.append(re.sub("[^A-Za-z ]","",str(netflix_titles.loc[netflix_titles['title'] == movie.lower().title()]['overview'].values)))
+    print('\n')
+    i = i + 1
 
-
-s2 = '\n'.join([str(i) for i in recolistdf.loc[:,"title"]])
-s = '\n'.join([str(i) for i in rec_list])
-sg.PopupScrolled("The following movies are recommended:", f"{s2}")
+s2 = '\n'.join([str(i) for i in final_list])
+#s = '\n'.join([str(i) for i in rec_list])
+sg.PopupScrolled("The Following Movies are Recommended:", "\n", f"{s2}")
 
 # testing
 #print('Values', values[0])
